640387f74adf8dd6d36e06078f71e8a53432399d
unknown <uptimevic@gmail.com>
2018-04-01 18:28:19 -0400

    bind

--- www/_chunk/setup.ts
+++ www/_chunk/setup.ts
@@ -7,12 +7,12 @@ loadjs([
 	,'//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
 	,'//cdn.jsdelivr.net/npm/signals@1.0.0/dist/signals.min.js'
 
-	], 'PRELoadBx123', { //bundle name: loadjs.ready('PRELoadB', function() { 
+	], 'PRELoadBx123', { //bundle name: loadjs.ready('PRELoadB', function() {
 			async: false //required due to loadjs bug with bundles
 		}
 )
 
-loadjs.ready('PRELoadBx123', function() { 
+loadjs.ready('PRELoadBx123', function() {
 	window['SITE'] = new signals.Signal() //site events
 	console.log('site setup')
 	loadjs.done('PRELoadB')
