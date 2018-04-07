bf05e4802e60e91ddda46b93cf7571a43a32b4bc
unknown <uptimevic@gmail.com>
2018-04-07 17:21:13 -0400

    mark

--- www/linkBlog/items-tag.js
+++ www/linkBlog/items-tag.js
@@ -1,10 +1,10 @@
 
-riot.tag2('items-tag', '<p>in tag</p><virtual each="{items}"> <p>{title} {content_text}</p></virtual>', '', '', function(opts) {
+riot.tag2('items-tag', '<virtual each="{items}"> <h3>{title}</h3> <div>{content_text}</div></virtual> <p></p> <p></p>', '', '', function(opts) {
     console.log('items tag')
     this.items = []
 
      this.renderItems = function(args) {
-    	console.log('XXX '+ JSON.stringify(args[0]))
+    	console.log('first row '+ JSON.stringify(args[0]))
     	this.items = args
     	this.update()
     }.bind(this)
