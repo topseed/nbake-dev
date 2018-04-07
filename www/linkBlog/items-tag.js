6f7d8cc9c4f0fcfc0d2854d418263d595e251e13
unknown <uptimevic@gmail.com>
2018-04-07 18:13:15 -0400

    done, now do over.

--- www/linkBlog/items-tag.js
+++ www/linkBlog/items-tag.js
@@ -1,5 +1,5 @@
 
-riot.tag2('items-tag', '<div class="ui cards"><virtual each="{items}"> <div class="card"> <image><img riot-src="{image}" width="160"></image> <div class="content"> <div class="header">{title}</div> <div class="meta">{author}</div> <div class="description">{content_text}</div> </div> </div></virtual> </div> <p></p> <p></p>', '', '', function(opts) {
+riot.tag2('items-tag', '<virtual each="{items}"><a href="{external_url}"> <div class="ui card"> <image><img riot-src="{image}" width="160"></image> <div class="content"> <div class="header">{title}</div> <div class="meta">{author}</div> <div class="description">{content_text}</div> </div> </div></a> <p></p></virtual> <p></p> <p></p>', '', '', function(opts) {
     console.log('items tag')
     this.items = []
 
