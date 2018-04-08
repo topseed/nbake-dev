8e975037c54db7dfce920e692cf5733d24333e1e
unknown <uptimevic@gmail.com>
2018-04-08 17:56:28 -0400

    added semi

--- www/assets/js/setup.js
+++ www/assets/js/setup.js
@@ -2,7 +2,7 @@
  // http://github.com/muicss/loadjs/issues/56
 
  // https://jsfiddle.net/muicss/4791kt3w
- function require(bundleIds, callbackFn) {
+function require(bundleIds, callbackFn) {
 	bundleIds.forEach(function(bundleId) {
 		if (!loadjs.isDefined(bundleId)) loadjs(bundles[bundleId], bundleId)
 	})
@@ -33,37 +33,30 @@ loadjs.ready(['promise','fetch'], function () {
 		'//cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js'
 		,'//cdn.jsdelivr.net/npm/signals@1.0.0/dist/signals.min.js'
 		,'//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
-		, '//cdn.jsdelivr.net/npm/riot@3.9.1/riot.js'
+		//, '//cdn.jsdelivr.net/npm/riot@3.9.1/riot.js'
 	], 'core' /* bundle ID */, {
 		async: false //required due to loadjs bug with bundles
 	})
 })
 
-WebFontConfig = {
-	google: { families: ['Open Sans:300'] }
-	, active: function() {
-		loadjs.done('font') // font is active
-	}
-}
-
 loadjs.ready(['core'], function () {
-	WebFont.load( WebFontConfig )
 	//window['SITE'] = new signals.Signal() //site events
 	loadjs([ '//cdn.jsdelivr.net/npm/semantic-ui@2.3.0/dist/semantic.js'
 	], 'cssJs', {
 		async: false //required due to loadjs bug with bundles
 	})
-
-	loadjs.done('site') // "done with bundle 'site'", need this because we're not loading js here
+	setTimeout(function(){
+		loadjs.done('site') // "done with bundle 'site'", need this because we're not loading js here
+	}, 1)
 })
 
 function cssLoaded() {// called by the style sheet in layout
 	console.log('css done')
-	loadjs.done('css')
+	loadjs.done('css') 
 }
 
-loadjs.ready(['css', 'cssJs', 'font'], function () {
-	loadjs.done('style')
+loadjs.ready(['css', 'cssJs'], function () {
+	loadjs.done('style') 
 })
 
 // usage: ////////////////////////////////////////////////////////////////////
